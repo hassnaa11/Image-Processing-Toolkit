a016f3d5730a078e947c82107710df8d2792c35a
Emaaanabdelazeemm <126925323+Emaaanabdelazeemm@users.noreply.github.com>
2025-02-28 15:17:53 +0200

    edge

--- Image.py
+++ Image.py
@@ -1,26 +1,29 @@
 import cv2
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import QGraphicsScene
+from PyQt5.QtGui import *
+import cv2
 import numpy as np
-from scipy import ndimage
+
 class Image:
-    def __init__(self, path):
+    def __init__(self):
+        self.image = None    
+    
+    def read_image(self, path):
         self.image_path = path
-        self.read_image()
-    
-    
-    def read_image(self):
         self.image = cv2.imread(self.image_path)    
         self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
-       
         
+
     def display_image(self):
-        height, width, channel = self.image.shape
-        bytes_per_line = channel * width
-        
-        # convert image to QImage
-        q_image = QImage(self.image, width, height, bytes_per_line, QImage.Format_RGB888) 
-        
+        height, width = self.image.shape[:2]
+        if len(self.image.shape) == 2: # gray-scale image
+            bytes_per_line = width
+            q_image = QImage(self.image, width, height, bytes_per_line, QImage.Format_Grayscale8)
+        else:  # RGB image 
+            bytes_per_line = 3 * width 
+            q_image = QImage(self.image, width, height, bytes_per_line, QImage.Format_RGB888)
+
         # convert QImage to QPixmap
         pixmap = QPixmap.fromImage(q_image) 
         
@@ -29,32 +32,7 @@ class Image:
         scene.addPixmap(pixmap)
         return scene
         
-        
-    def add_noise(self, noise_type, parameters):
-        if noise_type == 'Uniform':
-            min_range, max_range = parameters[0], parameters[1]
-            noise = np.random.randint(min_range, max_range, self.image.shape).astype(np.int16)
-            self.image = np.clip(self.image.astype(np.int16) + noise, 0, 255).astype(np.uint8)
-        
-        elif noise_type == 'Gaussian':
-            mean, sigma = parameters[0], parameters[1]
-            gauss = np.random.normal(mean, sigma, self.image.shape).astype(np.uint8)
-            self.image = cv2.add(self.image, gauss)
-        
-        elif noise_type == 'Salt & Pepper':
-            # print("parameters ", parameters)
-            ratio, probability = parameters[0], parameters[1]
-            num_salt = np.ceil(probability * ratio * self.image.size)
-            num_pepper = np.ceil(probability * (1 - ratio) * self.image.size)
-            # print("num_salt:  ", num_salt,"num_pepper: ", num_pepper)
-            # print( "self.image.size: ", self.image.size)
-            # print("self.image.shape[:2]:  ", self.image.shape[:2])
-
-            coordinates = [np.random.randint(0, i , int(num_salt)) for i in self.image.shape[:2]]
-            self.image[coordinates[0],coordinates[1], :] = 255
-            
-            coordinates = [np.random.randint(0, i , int(num_pepper)) for i in self.image.shape[:2]]
-            self.image[coordinates[0],coordinates[1], :] = 0
+    
     def apply_kernel(self, kernel, image=None):
         # Use the preserved image if no specific image is passed
         if image is None:
@@ -145,5 +123,8 @@ class Image:
         return self.display_image()
         
 
-
-                        
+    def get_image(self):
+        return np.copy(self.image)
+ 
+            
+                            
--- program.py
+++ program.py
@@ -1,12 +1,13 @@
 from PyQt5 import QtWidgets, QtCore, uic 
 import sys
 from PyQt5.QtGui import *
-from Image import Image
 import numpy as np
 import cv2
 
-import numpy as np
-from scipy import ndimage
+from Image import Image
+
+kernel_sizes = [3, 5, 7]
+
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
@@ -20,25 +21,35 @@ class MainWindow(QtWidgets.QMainWindow):
         
         # noises checkbox
         self.noises_combobox.setDisabled(True)
-        self.noises_combobox.currentIndexChanged.connect(self.add_noise)
+        self.noises_combobox.currentIndexChanged.connect(self.apply_changes)
         
         # filters checkbox
         self.filters_combobox.setDisabled(True)
-        self.filters_combobox.currentIndexChanged.connect(self.add_filter)
+        self.filters_combobox.currentIndexChanged.connect(self.apply_changes)
         
         # sliders
-        self.min_range_slider.valueChanged.connect(self.add_noise)
-        self.max_range_slider.valueChanged.connect(self.add_noise)
-        self.mean_slider.valueChanged.connect(self.add_noise)
-        self.sigma_slider.valueChanged.connect(self.add_noise)
-        self.probability_slider.valueChanged.connect(self.add_noise)
-        self.ratio_slider.valueChanged.connect(self.add_noise)
+        self.min_range_slider.valueChanged.connect(self.apply_changes)
+        self.max_range_slider.valueChanged.connect(self.apply_changes)
+        self.mean_slider.valueChanged.connect(self.apply_changes)
+        self.sigma_slider.valueChanged.connect(self.apply_changes)
+        self.probability_slider.valueChanged.connect(self.apply_changes)
+        self.ratio_slider.valueChanged.connect(self.apply_changes)
         self.min_range_slider.setDisabled(True)
         self.max_range_slider.setDisabled(True)
         self.show_hide_parameters('Uniform')
+        
+        # kernel size
+        self.kernel_index = 0
+        self.kernel_size_slider.valueChanged.connect(self.change_kernel)
 
         #edge detection
         self.edge_filters_combobox.currentIndexChanged.connect(self.apply_edge_detection_filter)
+        
+        # equalize button
+        self.equalization_button.clicked.connect(self.equalize_image)
+        # convert to grayscale
+        self.gray_scale_button.clicked.connect(self.convert_to_grayscale)
+        self.is_gray_scale = False
         
         
     def upload_image(self, key):  
@@ -46,12 +57,13 @@ class MainWindow(QtWidgets.QMainWindow):
             self, "Select Image", "", "Images (*.png *.jpg *.jpeg *.bmp *.gif)"
         )
         if self.file_path:
-            self.original_image = Image(self.file_path)
-            self.noisy_image = Image(self.file_path) 
-            self.filtered_image = Image(self.file_path)
-          
+            self.processor = Image()
+            self.processor.read_image(self.file_path)
+            self.noisy_image = Image()  # shelehom ya eman ma3lsh
+            self.noisy_image.read_image(self.file_path)
+            self.original_image = np.copy(self.processor.image)
             
-            scene = self.original_image.display_image()
+            scene = self.processor.display_image()
             if key == 1:
                 self.input_image.setScene(scene) 
                 self.input_image.fitInView(scene.sceneRect(), QtCore.Qt.KeepAspectRatio)  
@@ -59,7 +71,6 @@ class MainWindow(QtWidgets.QMainWindow):
                 self.filters_combobox.setDisabled(False)
                 self.min_range_slider.setDisabled(False)
                 self.max_range_slider.setDisabled(False)
-                self.add_noise()
             elif key == 2:
                 self.input1_image.setScene(scene)
                 self.input1_image.fitInView(scene.sceneRect(), QtCore.Qt.KeepAspectRatio)
@@ -68,11 +79,31 @@ class MainWindow(QtWidgets.QMainWindow):
                 self.input2_image.fitInView(scene.sceneRect(), QtCore.Qt.KeepAspectRatio)
     
     
-    def add_noise(self):
-        if not self.original_image:
-            return
+    def apply_changes(self):
+        kernel_size = kernel_sizes[self.kernel_index]
+        if self.processor and self.original_image is not None:
+            modified_image = np.copy(self.original_image)
+
+            # Apply noise if selected
+            noise_type = self.noises_combobox.currentText()
+            if noise_type != "None":
+                parameters = self.get_noise_parameters(noise_type)
+                noise_adder = NoiseAdder(modified_image)
+                modified_image = noise_adder.apply_noise(noise_type, parameters)
+
+            # Apply filter if selected
+            filter_type = self.filters_combobox.currentText()
+            if filter_type != "None":
+                filter_processor = FilterProcessor(modified_image)
+                modified_image = filter_processor.apply_filter(filter_type, kernel_size)
+
+            self.processor.image = modified_image
+            scene = self.processor.display_image()
+            self.output_image.setScene(scene) 
+            self.output_image.fitInView(scene.sceneRect(), QtCore.Qt.KeepAspectRatio) 
         
-        selected_noise = self.noises_combobox.currentText()
+    
+    def get_noise_parameters(self, selected_noise):
         self.show_hide_parameters(selected_noise)
         parameters = []
         
@@ -80,7 +111,7 @@ class MainWindow(QtWidgets.QMainWindow):
             min_range, max_range = self.min_range_slider.value(), self.max_range_slider.value()
             parameters = [min_range, max_range]
             self.min_range_label.setText(f"Minimum Range: {min_range}")
-            self.max_range_label.setText(f"Minimum Range: {max_range}")
+            self.max_range_label.setText(f"Maximum Range: {max_range}")
             
         elif selected_noise == 'Gaussian': 
             mean, sigma = self.mean_slider.value(), self.sigma_slider.value()
@@ -92,36 +123,42 @@ class MainWindow(QtWidgets.QMainWindow):
             ratio, probability = self.ratio_slider.value(),self.probability_slider.value()
             parameters = [ratio/10, probability/10]  
             self.ratio_label.setText(f"Ratio: {ratio/10}")
-            self.probability_label.setText(f"Probability: {probability/10}")   
-            
-        self.noisy_image.image = np.copy(self.original_image.image)   
-        self.noisy_image.add_noise(selected_noise, parameters)
-        scene = self.noisy_image.display_image()
-        self.output_image.setScene(scene) 
-        self.output_image.fitInView(scene.sceneRect(), QtCore.Qt.KeepAspectRatio)  
-    
-    
-    
-    def add_filter(self):
-        if not self.original_image:
-            return
-
-        # image_array = self.processed_image.image
-        self.filtered_image.image = np.copy(self.noisy_image.image)
-        image_array = self.filtered_image.image  
-        selected_filter = self.filters_combobox.currentText()
-        if selected_filter == 'Average':
-            filtered_image  = cv2.blur(image_array, (5,5))
-        elif selected_filter == 'Gaussian ':
-            filtered_image = cv2.GaussianBlur(image_array, (5,5), 0)
-        elif selected_filter == 'Median':
-            filtered_image = cv2.medianBlur(image_array, 5)
+            self.probability_label.setText(f"Probability: {probability/10}")
         
-        self.filtered_image.image = filtered_image
-        scene = self.filtered_image.display_image()
-        self.output_image.setScene(scene) 
-        self.output_image.fitInView(scene.sceneRect(), QtCore.Qt.KeepAspectRatio)     
+        return parameters    
     
+    
+    def equalize_image(self):
+        modified_image = np.copy(self.original_image)
+        filter_processor = FilterProcessor(modified_image)
+        modified_image = filter_processor.histogram_equalization()
+        self.processor.image = modified_image
+        scene = self.processor.display_image()
+        self.output_image.setScene(scene) 
+        self.output_image.fitInView(scene.sceneRect(), QtCore.Qt.KeepAspectRatio) 
+    
+    
+    def convert_to_grayscale(self):
+        modified_image = np.copy(self.original_image)
+        filter_processor = FilterProcessor(modified_image)
+        self.is_gray_scale = not self.is_gray_scale
+        
+        if self.is_gray_scale:
+            self.rgb_image = np.copy(self.original_image)
+            self.gray_scale_button.setText("Original") 
+            modified_image = filter_processor.rgb_to_grayscale()
+        else: 
+            self.gray_scale_button.setText("GrayScale") 
+            modified_image = np.copy(self.rgb_image)
+
+        self.original_image = np.copy(modified_image)
+        self.apply_changes()
+        
+        
+    def change_kernel(self):
+        self.kernel_index = self.kernel_size_slider.value()
+        self.kernel_size_label.setText(f"Kernel Size: {kernel_sizes[self.kernel_index]}x{kernel_sizes[self.kernel_index]}")
+        self.apply_changes()
     
     def show_hide_parameters(self, selected_noise):
         if selected_noise == 'Uniform':
